ffb9d008830e0b927abc0f661609fcfecec54f00
demir-bayrakgil <denizbayrakgil6@gmail.com>
2024-11-24 09:51:59 +0300

    .

--- demir.py
+++ demir.py
@@ -1,4 +1,4 @@
-print('demir makinesine hoş geldiniz 😊')
+print('demir  makinesine hoş geldiniz 😊')
 islem=input('istediğiniz işlemi numaralayınız toplama=1,çıkarma=2,çarpma=3,bölme=2')
 birincisayi=int(input('ilk sayıyı lütfen giriniz'))
 ikincisayi=int(input('ikinci sayıyı lütfen giriniz'))
